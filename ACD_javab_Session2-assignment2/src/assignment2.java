26c2a3d1844d874a7d0437f749425f11037dd1b1
u174762 <sam.panga@hotmail.com>
2016-08-04 21:45:43 +0530

    Add files via upload

---
+++ ACD_javab_Session2-assignment2/src/assignment2.java
@@ -0,0 +1,24 @@
+
+
+
+	public class assignment2 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			 int i ;
+		       int j ;
+		     
+		   
+		    for(i = 1;i <=100;i++){
+		    	for(j=2;j<=i;j++){
+		    	
+		    	if(j==i){
+			System.out.println(i);
+		}
+		else if(i%j==0){
+		
+			break;
+		}
+	}
+	}}}
